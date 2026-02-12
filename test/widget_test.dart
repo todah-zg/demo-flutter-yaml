6dc260137368913ac5afbe4673f0a33bb6109fbe
todah <104134795+todah-zg@users.noreply.github.com>
2026-02-12 14:26:52 +0100

    Update import path for main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ivanyaml/main.dart';
+import 'package:marijan_demo_flutter_yaml/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
